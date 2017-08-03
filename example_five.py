3d6044ac0c34f174744c54ae99f2e47564ebd6c5
David <davidwfallis@gmail.com>
2017-08-03 10:51:48 -0700

    fixed

--- example_five.py
+++ example_five.py
@@ -2,8 +2,12 @@ import re
 
 sentence = "Hello. My name is David. I am 26 years old."
 
+
 match = re.match('.*David.\\sI', sentence)
 match2 = re.match(r'.*David.\\sI', sentence)
+match3 = re.match(r'.*David.\sI', sentence)
+match4 = re.match('.*David.\sI', sentence)
+
 
 if(match):
     print("match1")
@@ -11,3 +15,9 @@ if(match):
 if(match2):
     print("match2")
     print(match2.group(0))
+if(match3):
+    print("match3")
+    print(match3.group(0))
+if(match4):
+    print("match4")
+    print(match4.group(0))
